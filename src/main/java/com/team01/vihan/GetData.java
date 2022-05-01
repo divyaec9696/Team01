4725fe7ed30ffd3ae680a42f7fd84899842298d2
divyaec9696 <divyaec9696@gmail.com>
2022-05-01 14:56:58 +0530

    new method added

---
+++ src/main/java/com/team01/vihan/GetData.java
@@ -0,0 +1,11 @@
+package com.team01.vihan;
+
+public class GetData {
+
+	public void m1() {
+		System.out.println("m1 method of GetData classs");
+	}
+	public void m2() {
+		System.out.println("m2 method of GetData classs");
+	}
+}
