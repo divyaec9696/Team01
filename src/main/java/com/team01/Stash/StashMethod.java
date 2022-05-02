ead336148bf2cce7d08787d65e7a60cd6f01761f
divyaec9696 <divyaec9696@gmail.com>
2022-05-03 01:20:54 +0530

    vicky folder added

---
+++ src/main/java/com/team01/Stash/StashMethod.java
@@ -0,0 +1,12 @@
+package com.team01.Stash;
+
+public class StashMethod {
+	
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=10;
+		System.out.println("multipliction is:"+a*b);
+	}
+
+}
